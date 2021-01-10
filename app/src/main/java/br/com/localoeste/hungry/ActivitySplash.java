cfd0c7c86aaacc9e379908c2c8491bcb225579d0
iranildo <iranildocarlos@gmail.com>
2021-01-10 20:11:20 -0300

    Definido as cores padrões

---
+++ app/src/main/java/br/com/localoeste/hungry/ActivitySplash.java
@@ -0,0 +1,14 @@
+package br.com.localoeste.hungry;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivitySplash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
