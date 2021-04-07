8c2be7bc02f63393f740f40ef6938e72d6640331
iranildo <iranildocarlos@gmail.com>
2021-04-07 00:29:39 -0300

    configurações da foto perfilEmpresa

--- app/src/main/java/br/com/localoeste/hungry/activy/ConfiguracaoEmpresaActivity.java
+++ app/src/main/java/br/com/localoeste/hungry/activy/ConfiguracaoEmpresaActivity.java
@@ -1,11 +1,20 @@
 package br.com.localoeste.hungry.activy;
 
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Spinner;
 
 import br.com.localoeste.hungry.R;
@@ -15,8 +24,12 @@ public class ConfiguracaoEmpresaActivity extends AppCompatActivity {
     private Spinner spinner;
     private Spinner spinnerInicio;
     private Spinner spinnerFinal;
+    private EditText editEmpresaNome, getEditEmpresaTaxa,getGetEditEmpresaCategoria, getEditEmpresaTempo;
+    private ImageView imagePerfilEmpresa;
+    private static final int SELECAO_GALERIA = 200;
 
 
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,8 +42,51 @@ public class ConfiguracaoEmpresaActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         inicializarComponentes();
+
+        imagePerfilEmpresa.setOnContextClickListener(new View.OnContextClickListener() {
+            @Override
+            public boolean onContextClick(View v) {
+                Intent i = new Intent(Intent.ACTION_PICK,
+                        MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+
+                if (i.resolveActivity(getPackageManager()) != null){
+                    startActivityForResult(i, SELECAO_GALERIA);
+                }
+                return false;
+            }
+        });
+
+
+
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK){
+            Bitmap imagem = null;
+            try {
+
+                switch (resultCode){
+                    case SELECAO_GALERIA:
+                        Uri localImagem = data.getData();
+                        imagem = MediaStore.Images
+                                .Media
+                                .getBitmap(
+                                        getContentResolver(),
+                                        localImagem
+                                );
+                        break;
+                }
+                if (imagem != null){
+                        imagePerfilEmpresa.setImageBitmap(imagem);
+                }
+
+            }catch (Exception erro){
+                erro.printStackTrace();
+            }
+        }
+    }
 
     private void inicializarComponentes() {
         spinner = findViewById(R.id.spinnerTeste);
@@ -38,6 +94,7 @@ public class ConfiguracaoEmpresaActivity extends AppCompatActivity {
         spinnerFinal = findViewById(R.id.spinnerHorarioFinal);
         String[] categorias = getResources().getStringArray(R.array.nome_categoria);
         String[] horarios = getResources().getStringArray(R.array.horario);
+        imagePerfilEmpresa = findViewById(R.id.image_perfil_empresa);
 
         ArrayAdapter<String> adapterHorario = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item,
                 horarios);
